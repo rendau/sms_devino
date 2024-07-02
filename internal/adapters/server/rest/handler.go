121c2e4f6ce642667023f89590daf91a72f21164
mdb <dauren.asir@gmail.com>
2024-07-02 09:15:39 +0500

    some changes

--- internal/adapters/server/rest/handler.go
+++ internal/adapters/server/rest/handler.go
@@ -1,16 +1,25 @@
 package rest
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	dopHttps "github.com/rendau/dop/adapters/server/https"
+
 	"github.com/rendau/sms_devino/internal/domain/entities"
 )
 
 func (a *St) hSend(c *gin.Context) {
+	reqBody, err := c.GetRawData()
+	if dopHttps.Error(c, err) {
+		return
+	}
+
 	reqObj := &entities.SendReqSt{}
-	if !dopHttps.BindJSON(c, reqObj) {
+	err = json.Unmarshal(reqBody, reqObj)
+	if dopHttps.Error(c, err) {
+		a.lg.Infow("Fail to unmarshal request", "err", err, "reqBody", string(reqBody))
 		return
 	}
 
